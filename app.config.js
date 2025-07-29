969c87c46b96b88326768b1b0973022beb8c40ec
mosodi007 <martins.osodi@gmail.com>
2025-07-29 10:07:03 +0100

    Added app.json

---
+++ app.config.js
@@ -0,0 +1,37 @@
+// import { ExpoConfig, ConfigContext } from 'expo/config';
+
+// app.config.js
+export default {
+  expo: {
+    name: 'Planmoni',
+    owner: "planmoni", // 👈 Add this line
+    slug: "planmoni",
+    version: "1.0.0",
+    scheme: "myapp",
+    android: {
+      package: "com.planmoni.app", // ← choose your unique package name
+      "permissions": ["android.permission.CAMERA"]
+    },
+    ios: {
+      "bundleIdentifier": "com.planmoni"
+    },
+    extra: {
+      "eas": {
+        "projectId": "05caad20-9b74-4ba8-8280-dc5939b7ca83"
+      },
+      EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL || '',
+      EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || '',
+      EXPO_PUBLIC_PAYSTACK_PUBLIC_KEY: process.env.EXPO_PUBLIC_PAYSTACK_PUBLIC_KEY || '',
+      EXPO_PUBLIC_MONO_PUBLIC_KEY: process.env.EXPO_PUBLIC_MONO_PUBLIC_KEY || '',
+      EXPO_PUBLIC_MONO_SECRET_KEY: process.env.EXPO_PUBLIC_MONO_SECRET_KEY || '',
+      // Use platform-specific API URL handling
+      EXPO_PUBLIC_API_URL: process.env.EXPO_PUBLIC_API_URL || '',
+      PAYSTACK_SECRET_KEY: process.env.PAYSTACK_SECRET_KEY || '',
+      EXPO_PUBLIC_PAYSTACK_SECRET_KEY: process.env.EXPO_PUBLIC_PAYSTACK_SECRET_KEY || '',
+      EXPO_PUBLIC_DOJAH_APP_ID: process.env.EXPO_PUBLIC_DOJAH_APP_ID || '',
+      EXPO_PUBLIC_DOJAH_PRIVATE_KEY: process.env.EXPO_PUBLIC_DOJAH_PRIVATE_KEY || '',
+      EXPO_PUBLIC_PAYSTACK_LIVE_SECRET_KEY: process.env.EXPO_PUBLIC_PAYSTACK_LIVE_SECRET_KEY || '',
+      EXPO_PUBLIC_OPENAI_API_KEY: process.env.EXPO_PUBLIC_OPENAI_API_KEY || '',
+    },
+  },
+};
